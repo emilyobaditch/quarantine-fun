173bc84b71d191a2cf28e2e90ad01ab98b22df4c
Emily Obaditch <emily@loftsmart.com>
2020-03-23 19:08:22 -0400

    routing stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import Home from './Home'
 
 function App() {
   return (
     <div title="countup-quaratine">
-      <Home />
+      <BrowserRouter basename={window.location.pathname || ''}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   )
 }
