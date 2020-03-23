aa5ceeed68c83a2250d6cbe0cab9b2c918352cf3
emilyobaditch <56836081+emilyobaditch@users.noreply.github.com>
2020-03-23 23:10:24 +0000

    Deploying to gh-pages from  - 173bc84b71d191a2cf28e2e90ad01ab98b22df4c 🚀

---
+++ precache-manifest.ea9b1101104955be97062d277717b8bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0364867e8b7f853453ca25fd00115d46",
+    "url": "/quarantine-fun/index.html"
+  },
+  {
+    "revision": "db387e0619f2a57d3920",
+    "url": "/quarantine-fun/static/css/2.bc6f96dc.chunk.css"
+  },
+  {
+    "revision": "9deeea034125cb67a15f",
+    "url": "/quarantine-fun/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "db387e0619f2a57d3920",
+    "url": "/quarantine-fun/static/js/2.f5fb2ae0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/quarantine-fun/static/js/2.f5fb2ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9deeea034125cb67a15f",
+    "url": "/quarantine-fun/static/js/main.af886901.chunk.js"
+  },
+  {
+    "revision": "d8e48a5a68a636ca3cf6",
+    "url": "/quarantine-fun/static/js/runtime-main.ff8e78e0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quarantine-fun/precache-manifest.44837a7aecc1a01041f487b2d1d1f96c.js"
+  "/quarantine-fun/precache-manifest.ea9b1101104955be97062d277717b8bc.js"
 );
 
 self.addEventListener('message', (event) => {
